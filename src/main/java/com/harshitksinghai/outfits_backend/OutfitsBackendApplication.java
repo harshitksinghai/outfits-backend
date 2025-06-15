635d7418376b82bdd951c56ed754463a75be4b5e
harshitksinghai <harshitksinghai.official@gmail.com>
2025-06-15 13:57:12 +0530

    first commit

---
+++ src/main/java/com/harshitksinghai/outfits_backend/OutfitsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.harshitksinghai.outfits_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutfitsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutfitsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harshitksinghai/outfits_backend/OutfitsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshitksinghai.outfits_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutfitsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
